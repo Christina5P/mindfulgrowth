a06230971d1ddd8f60880cd3826d5daa355429d7
Christina Åhman <christina.ahman@tv4.se>
2024-07-04 14:40:03 +0000

    change template path

--- basic_mind/settings.py
+++ basic_mind/settings.py
@@ -51,7 +51,8 @@ ROOT_URLCONF = 'basic_mind.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [os.path.join.(BASE_DIR, 'templates'),
+        os.path.join(BASE_DIR, 'templates', 'allauth'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
