d337a7bd0c84fa70991f6e910309f8aff2d7e061
Christina Åhman <christina.ahman@tv4.se>
2024-07-08 09:34:21 +0000

    add view functions

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from .models import Category
 
 # Register your models here for administrative interface
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Category
+from .models import Post, Comment
 
 
-# Create your views here.
-def my_blog(request):
+# View function to display post in different ways
+
+def blog_index(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }
     return HttpResponse("Hello, Blog!")
+    # return to change to render from right html:
+    #  return render(request, "blog/index.html", context)
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-created_on")
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "blog/category.html", context)
+    
+
+    def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog/detail.html", context)
