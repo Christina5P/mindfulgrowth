90925ff6f7e7dd2f1a9e1a5bd6ae69cd3a9e6e92
Christina Åhman <christina.ahman@tv4.se>
2024-07-08 19:38:57 +0000

    add summernote, forloops in category.html index.html

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
-from .models import Post, Comment
-from .models import Category
+from .models import Post, Comment, Category
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here for administrative interface
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_on', 'last_modified')
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'created_on')
+    search_fields = ['title', 'content']
     filter_horizontal = ('categories',)
-
+    summernote_fields = ('content',)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at', 'updated_at')
+
+admin.site.register(Comment)
