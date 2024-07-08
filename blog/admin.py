8454023d561e80cbb14304d7c7e0da152e716952
Christina Åhman <christina.ahman@tv4.se>
2024-07-08 08:04:10 +0000

    correct list display

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from .models import Post
-from category.models import Category
+from .models import Category
 
 # Register your models here for administrative interface
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'updated_at')
+    list_display = ('title', 'created_on', 'last_modified')
     filter_horizontal = ('categories',)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'updated_at')
+    list_display = ('name', 'created_at', 'updated_at')
