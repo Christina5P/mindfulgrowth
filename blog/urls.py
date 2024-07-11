4fe585bcbd86f94b35dc7597652fb82fc602bbbe
Christina Åhman <christina.ahman@tv4.se>
2024-07-11 09:16:56 +0000

    forgot save files before commit

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from.views import post
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    #path('<slug:slug>/', views.post, name='post'),
     path("post/<int:pk>/", views.blog_detail, name ="blog_category" ),
     path("category/<category>/", views.blog_category, name="blog_category"),
     path('search/', views.category_search, name='category_search'),
